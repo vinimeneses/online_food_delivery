b1fd4a3ec57748355cd0e48182577cdee73d3c1e
vinimeneses <viniciusmenesesdev@gmail.com>
2024-05-23 12:34:49 -0300

    first commit

---
+++ src/main/java/com/vinimeneses/delivery_food/DeliveryFoodApplication.java
@@ -0,0 +1,13 @@
+package com.vinimeneses.delivery_food;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveryFoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeliveryFoodApplication.class, args);
+	}
+
+}
